91e74aa4c07b71c28479a9fa248ee99bfb061509
Swagoto <saswagoto@gmail.com>
2024-01-30 19:55:27 +0600

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,12 @@ const iconToggle = (icon1, icon2) => {
     icon1.classList.toggle('d-none');
     icon2.classList.toggle('d-none');
 };
+//
 
+
+const closeBtn = document.querySelector('.closeBtn');
+const notifyPart = document.querySelector('.notification');
+
+closeBtn.addEventListener('click', ()=>{
+    notifyPart.classList.add('hidden');
+});
