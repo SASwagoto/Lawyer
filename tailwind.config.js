16ed292180e4f76314305357ad9b1754b4a4fa73
Swagoto <saswagoto@gmail.com>
2024-01-31 16:59:49 +0600

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,34 @@
 module.exports = {
   content: ['*'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        brand: ['Bai Jamjuree']
+      },
+      animation: {
+        slider : 'slider 4s infinite'
+      },
+      keyframes:{
+        slider : {
+          '0%' : {
+            transform : "translateX(0px)",
+          },
+          '25%' : {
+            transform : "translateX(250px)",
+          },
+          '50%' : {
+            transform : "translateX(400px)",
+          },
+          '75%' : {
+            transform : "translateX(250px)",
+          },
+          '100%' : {
+            transform : "translateX(0px)",
+          }
+        }
+      }
+
+    },
   },
   plugins: [],
 }
